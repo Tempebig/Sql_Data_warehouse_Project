ec3737dff08fc60f388f64ce98e1d1f25b4ed533
Hamdi Maki <155289688+Tempebig@users.noreply.github.com>
2025-05-28 12:49:34 +0300

    Update DDL_bronze.sql

--- scripts/bronze/DDL_bronze.sql
+++ scripts/bronze/DDL_bronze.sql
@@ -2,119 +2,76 @@
 create  bronze layer tables 
 
 */
+----customer infromation table 
+IF OBJECT_ID('bronze.crm_cust_info','U') IS NOT NULL
+ DROP TABLE bronze.crm_cust_info;
+create table bronze.crm_cust_info(
+ cst_id int,
+ cst_key nvarchar(50),
+ cst_fristname nvarchar(50),
+  cst_lastname nvarchar(50),
+   cst_material_status nvarchar(50),
+    cst_fgndr nvarchar(50),
+	cst_create_date date
 
-create or alter procedure bronze.load_bronze as 
-begin 
-   Declare @start_time Datetime, @end_time Datetime;
+);
 
-begin try 
-print'================================';
-print'Loading Bronze Layer';
-print'================================';
-print'--------------------------------';
-print 'Loading CRM Tables ';
-print'--------------------------------';
- set @start_time= GETDATE();
-	TRUNCATE TABLE BRONZE.crm_cust_info;
-	BULK INSERT BRONZE.crm_cust_info
-	FROM 'C:\Users\Deena\Desktop\sql\datasets\source_crm\cust_info.csv' 
-	WITH(
-	  FIRSTROW =2,
-	  FIELDTERMINATOR= ',',
-	  TABLOCK --- LOKIN TO INTAIER TABLE 
+----Product table 
+IF OBJECT_ID('bronze.crm_prd_info','U') IS NOT NULL
+ DROP TABLE bronze.crm_prd_info;
+create table bronze.crm_prd_info(
+ prd_id int,
+ prd_key nvarchar(50),
+ prd_nm nvarchar(50),
+ prd_cost int,
+prd_lin nvarchar(50),
+ 	prd_start_dt date,
+	prd_end_dt date
 
-	);
-	 set @end_time= GETDATE();
-	 print '>>> Load Duration : ' + cast( Datediff(second,@start_time,@end_time) as nvarchar) +' seconds';
-	 print '>>-------------------';
-	--- TEST THE TABLE 
-	---SELECT * FROM BRONZE.crm_cust_info;
-	--SELECT COUNT(*) FROM BRONZE.crm_cust_info;
-	 set @start_time= GETDATE();
-	TRUNCATE TABLE BRONZE.crm_prd_info;
+);
 
-	BULK INSERT BRONZE.crm_prd_info
-	FROM 'C:\Users\Deena\Desktop\sql\datasets\source_crm\prd_info.csv' 
-	WITH(
-	  FIRSTROW =2,
-	  FIELDTERMINATOR= ',',
-	  TABLOCK --- LOKIN TO INTAIER TABLE 
-
-	);
-	 set @end_time= GETDATE();
-	 print '>>> Load Duration : ' + cast( Datediff(second,@start_time,@end_time) as nvarchar) +' seconds';
-	 print '>>-------------------';
-
-	  set @start_time= GETDATE();
-	TRUNCATE TABLE BRONZE.crm_sales_details;
-	BULK INSERT BRONZE.crm_sales_details
-	FROM 'C:\Users\Deena\Desktop\sql\datasets\source_crm\sales_details.csv' 
-	WITH(
-	  FIRSTROW =2,
-	  FIELDTERMINATOR= ',',
-	  TABLOCK --- LOKIN TO INTAIER TABLE 
-
-	);
-	 set @end_time= GETDATE();
-	 print '>>> Load Duration : ' + cast( Datediff(second,@start_time,@end_time) as nvarchar) +' seconds';
-	 print '>>-------------------';
+---- sales Details table
+IF OBJECT_ID('bronze.crm_sales_details','U') IS NOT NULL
+ DROP TABLE bronze.crm_sales_details;
+create table bronze.crm_sales_details(
+sls_ord_num nvarchar(50),
+sls_prd_key nvarchar(50),
+sls_cust_id int,
+sls_order_dt nvarchar(50),
+sls_ship_dt nvarchar(50),
+sls_due_dt nvarchar(50),
+sls_sales int,
+sls_quantity int,
+sls_price int,
+);
 
 
-	----insert ERP data --------
-	 set @start_time= GETDATE();
-print'--------------------------------';
-print 'Loading ERP Tables ';
-print'--------------------------------';
-	TRUNCATE TABLE BRONZE.erp_CUST_AZ12;
-	BULK INSERT BRONZE.erp_CUST_AZ12
-	FROM 'C:\Users\Deena\Desktop\sql\datasets\source_erp\CUST_AZ12.csv' 
-	WITH(
-	  FIRSTROW =2,
-	  FIELDTERMINATOR= ',',
-	  TABLOCK --- LOKIN TO INTAIER TABLE 
+----------------------ERP Tables---------------------------
 
-	);
-	 set @end_time= GETDATE();
-	 print '>>> Load Duration : ' + cast( Datediff(second,@start_time,@end_time) as nvarchar) +' seconds';
-	 print '>>-------------------';
- 
-  set @start_time= GETDATE();
-	TRUNCATE TABLE BRONZE.erp_LOC_A101;
-	BULK INSERT BRONZE.erp_LOC_A101
-	FROM 'C:\Users\Deena\Desktop\sql\datasets\source_erp\LOC_A101.csv' 
-	WITH(
-	  FIRSTROW =2,
-	  FIELDTERMINATOR= ',',
-	  TABLOCK --- LOKIN TO INTAIER TABLE 
+IF OBJECT_ID('bronze.erp_CUST_AZ12','U') IS NOT NULL
+ DROP TABLE bronze.erp_CUST_AZ12;
+create table bronze.erp_CUST_AZ12(
+CID nvarchar(50),
+BDATE DATE,
+GEN nvarchar(50)
 
-	);
-	 set @end_time= GETDATE();
-	 print '>>> Load Duration : ' + cast( Datediff(second,@start_time,@end_time) as nvarchar) +' seconds';
-	 print '>>-------------------';
+);
+----------
+IF OBJECT_ID('bronze.erp_PX_CAT_G1V2','U') IS NOT NULL
+ DROP TABLE bronze.erp_PX_CAT_G1V2;
+create table bronze.erp_PX_CAT_G1V2(
+ID nvarchar(50),
+CAT nvarchar(50),
+SUBCAT nvarchar(50),
+MAINTENANCE nvarchar(50)
+);
+--------
+IF OBJECT_ID('bronze.erp_LOC_A101','U') IS NOT NULL
+ DROP TABLE bronze.erp_LOC_A101;
+create table bronze.erp_LOC_A101(
+CID nvarchar(50),
+CNTRY nvarchar(50),
 
-	  set @start_time= GETDATE();
-	TRUNCATE TABLE BRONZE.erp_PX_CAT_G1V2;
-	BULK INSERT BRONZE.erp_PX_CAT_G1V2
-	FROM 'C:\Users\Deena\Desktop\sql\datasets\source_erp\PX_CAT_G1V2.csv' 
-	WITH(
-	  FIRSTROW =2,
-	  FIELDTERMINATOR= ',',
-	  TABLOCK --- LOKIN TO INTAIER TABLE 
+);
+------------
 
-	);
-	 set @end_time= GETDATE();
-	 print '>>> Load Duration : ' + cast( Datediff(second,@start_time,@end_time) as nvarchar) +' seconds';
-	 print '>>-------------------';
-	end try
-	begin catch 
-	  print'============================'
-	  print'Error Occured During Loading Bronze Layer'
-	  print 'Error Messagr'+  Error_message();
-	  print 'Error Messagr'+ cast  (Error_number() as nvarchar);
-	   print 'Error Messagr'+ cast  (Error_state() as nvarchar);
-	  print'============================'
-	end catch
-end
-
-
-exec bronze.load_bronze
